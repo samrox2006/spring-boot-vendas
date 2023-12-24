d6b6efc91b55cce99a428ad4cd747909c4e2d77a
Bruno Mulin <verdibianco@hotmail.com>
2023-12-24 02:03:19 -0300

    arquivo renomeado

--- src/main/java/io/github/samrox2006/VendasApplication.java
+++ src/main/java/io/github/samrox2006/VendasApplication.java
@@ -1,4 +1,4 @@
-package io.github.brunosam;
+package io.github.samrox2006;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
